41d2dc95d90853871f99ff39be73318012fe078d
dmcarmen <dmcarmen1999@gmail.com>
2021-02-22 19:59:33 +0200

    4.3 4.4

---
+++ part4/bloglist/utils/list_helper.js
@@ -0,0 +1,12 @@
+const dummy = () => {
+  return 1
+}
+
+const totalLikes = (blogs) => {
+  return blogs.reduce((acc, curr) => acc + curr.likes, 0) || 0
+}
+
+module.exports = {
+  dummy,
+  totalLikes
+}

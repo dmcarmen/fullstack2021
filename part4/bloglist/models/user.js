0e78f4bc31d1809e64a6ac8e0df298109007bdc5
dmcarmen <dmcarmen1999@gmail.com>
2021-02-22 21:41:20 +0200

    more

--- part4/bloglist/models/user.js
+++ part4/bloglist/models/user.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
 
 const userSchema = new mongoose.Schema({
-  username: String,
+  username: { type: String, minlength: 3, unique: true },
   name: String,
   passwordHash: String,
   blogs: [
@@ -12,16 +13,15 @@ const userSchema = new mongoose.Schema({
   ],
 })
 
+userSchema.plugin(uniqueValidator)
+
 userSchema.set('toJSON', {
   transform: (document, returnedObject) => {
     returnedObject.id = returnedObject._id.toString()
     delete returnedObject._id
     delete returnedObject.__v
-    // the passwordHash should not be revealed
     delete returnedObject.passwordHash
   }
 })
 
-const User = mongoose.model('User', userSchema)
-
-module.exports = User
+module.exports = mongoose.model('User', userSchema)

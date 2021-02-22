de4ded4954db93d9353d249b1b02201668be7268
dmcarmen <dmcarmen1999@gmail.com>
2021-02-22 22:26:05 +0200

    compulsory

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -6,7 +6,15 @@ const totalLikes = (blogs) => {
   return blogs.reduce((acc, curr) => acc + curr.likes, 0) || 0
 }
 
+const favoriteBlog = (blogs) => {
+  const topBlog = blogs.reduce((acc, curr) => curr.likes > acc.likes ? curr : acc)
+  // Omit '_id', 'url' and '__v'
+  const { _id, url, __v, ...rest } = topBlog
+  return rest
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }

298b8a8d5721158ca0455b867c490342e41e254f
dmcarmen <dmcarmen1999@gmail.com>
2021-02-22 20:26:57 +0200

    medio

---
+++ part4/bloglist/tests/blog_api.test.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const api = supertest(app)
+const helper = require('./test_helper')
+
+const Blog = require('../models/blog')
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  let noteObject = new Blog(helper.initialBlogs[0])
+  await noteObject.save()
+  noteObject = new Blog(helper.initialBlogs[1])
+  await noteObject.save()
+})
+
+test('blogs are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+test('there are 2 blogs', async () => {
+  const response = await api.get('/api/blogs')
+  expect(response.body.length).toBe(2)
+})
+
+afterAll(() => {
+  mongoose.connection.close()
+})

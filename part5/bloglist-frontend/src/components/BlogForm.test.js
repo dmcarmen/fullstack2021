a7c64cc7ed92a3a2139bee9520bea455ca74157e
dmcarmen <dmcarmen1999@gmail.com>
2021-03-01 23:55:56 +0200

    finish

--- part5/bloglist-frontend/src/components/BlogForm.test.js
+++ part5/bloglist-frontend/src/components/BlogForm.test.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import { render, fireEvent } from '@testing-library/react'
+import { render, fireEvent, waitFor } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 import BlogForm from './BlogForm'
 
-test('<BlogForm /> updates parent state and calls onSubmit', () => {
+test('<BlogForm /> updates parent state and calls onSubmit', async () => {
   const createBlog = jest.fn()
 
   const component = render(
@@ -24,7 +24,9 @@ test('<BlogForm /> updates parent state and calls onSubmit', () => {
   fireEvent.change(url, {
     target: { value: 'moi.es' }
   })
-  fireEvent.submit(form)
+  await waitFor(() => {
+    fireEvent.submit(form)
+  })
 
   expect(createBlog.mock.calls).toHaveLength(1)
   expect(createBlog.mock.calls[0][0].title).toBe('testing of forms could be easier')
